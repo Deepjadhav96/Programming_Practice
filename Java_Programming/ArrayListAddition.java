72c0b2a986b881734a35201c39f81e1a88c67609
Deepraj <deepjadhav183@gmail.com>
2026-06-03 09:57:46 +0530

    ArrayList Addition

---
+++ Java_Programming/ArrayListAddition.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class ArrayListAddition
+{
+    public static void main(String A[])
+    {
+        ArrayList <Integer> aobj = new ArrayList<Integer>();
+
+        aobj.add(10);
+        aobj.add(20);
+        aobj.add(30);
+        aobj.add(40);
+
+        System.out.println(aobj);
+
+        Iterator iobj = aobj.iterator();
+
+        int iSum = 0;
+
+        while(iobj.hasNext())
+        {
+            iSum = iSum + (int)iobj.next();
+
+        }
+
+        System.out.println("Addition is: "+iSum);
+    }
+}
+
